921eaf3acb9009b994c4fa14fbf726236e05a5cd
Rio Foster <friotious@gmail.com>
2021-10-22 16:52:48 -0700

    Add VEHICLE object model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var VEHICLE = {
+  make: 'Toyota',
+  model: 'Tundra',
+  year: 2002,
+  miles: 239687,
+  has4wd: true,
+  hasBeenWashed: false,
+  extras: ['bedliner', 'fog lights', 'towhitch'],
+  needsRepair: ['Left rear axle', 'cabin fan', 'Left rear panel']
+}
