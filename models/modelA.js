7861370643d32d8ab96584cdac951ef1b2303898
Rio Foster <friotious@gmail.com>
2021-10-22 16:49:48 -0700

    Add AIRPORT object model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var AIRPORT = {
+  name: 'SeaTac International',
+  state: 'Wasington',
+  numOfTerminals: 11,
+  numOfRunways: 27,
+  hasLongtermParking: true,
+  hasDelaysToday: false,
+  airlines: ['American', 'Southwest', 'Alaskan', 'Delta'],
+  hotelsNearby: ['RedLion', 'Marriot', 'Best Western']
+}
