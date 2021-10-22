e401f1f21fefad8be68b4983291a3ca527c3ce9f
Rio Foster <friotious@gmail.com>
2021-10-22 16:28:45 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var famousPerson = 'Captain Hook'
+
+var isActive = false
+
+famousPerson = 'Peter Sellers'
+
+var num3 = 13
+var num9 = 19
+var sum = num9 + num3
+
+var animals = ['penguin', 'whale', 'cat']
+var removeFirstElement = animals.shift()
+
+console.log(famousPerson.length)
